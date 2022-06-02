d5cce57d5e95354eeb2b602917111b73b12f88b7
sojungpp <sophie2458@naver.com>
2022-06-02 16:02:01 +0900

    보험가입내역

--- src/domain/insurance/entity/AccidentInsurance.java
+++ src/domain/insurance/entity/AccidentInsurance.java
@@ -2,4 +2,8 @@ package domain.insurance.entity;
 
 public class AccidentInsurance extends Insurance{
 
+
+    public AccidentInsurance(String insuranceName, int fee) {
+        super(insuranceName, fee);
+    }
 }
