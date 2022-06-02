099f5116d08837d5ec8812c36d914e9875afec84
kyubeom <be_man@naver.com>
2022-06-02 17:51:39 +0900

    미 상담 고객 구현 진행중

--- src/domain/employee/repository/EmployeeRepository.java
+++ src/domain/employee/repository/EmployeeRepository.java
@@ -1,11 +1,17 @@
 package domain.employee.repository;
 
+import domain.customer.entity.Customer;
+import domain.customer.enumeration.KindOfJob;
+import domain.employee.dto.CustomerConsultResponse;
 import domain.employee.entity.Employee;
 import domain.employee.exception.excution.NoEmployeeException;
+import domain.insurance.entity.enumeration.KindOfInsurance;
 import global.util.Constants;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+
 /*
 * 여기에 CRUD 있어요~ argument만 바꿔서 쓰면됨
 * */
@@ -15,6 +21,21 @@ public class EmployeeRepository {
 
     public EmployeeRepository() {
         this.connection = this.sqlConnection();
+    }
+
+    private Connection sqlConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = null;
+            conn = DriverManager.getConnection(
+                    Constants.URL,
+                    Constants.USER,
+                    Constants.PW);
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public String insert(Employee employee) throws IOException {
@@ -108,7 +129,6 @@ public class EmployeeRepository {
             st.setInt(1, employeeId);
             int result = st.executeUpdate();
             st.close();
-//            conn.close();
         }catch(SQLException e){
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -149,18 +169,59 @@ public class EmployeeRepository {
         return null;
     }
 
-    private Connection sqlConnection(){
+    public ArrayList<CustomerConsultResponse> customerConsult(Employee employee) {
+        ResultSet rs = null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = null;
-            conn = DriverManager.getConnection(
-                    Constants.URL,
-                    Constants.USER,
-                    Constants.PW);
-            return conn;
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+            String sql = "Select Emp_Cus.emp_CusId, Customer.customerId, Customer.name, Customer.phoneNumber, Customer.kindOfInsurance, Customer.kindOfJob" +
+                    " from Emp_Cus, Customer " +
+                    "where Customer.customerId = Emp_Cus.customerId and Emp_Cus.satisfaction is null;";
+            PreparedStatement st = this.connection.prepareStatement(sql);
+
+            rs = st.executeQuery();
+
+            ArrayList<CustomerConsultResponse> customerList = new ArrayList<>();
+            while (rs.next()){
+                CustomerConsultResponse customerConsultResponse = new CustomerConsultResponse();
+                customerConsultResponse.setEmpCusId(rs.getString("emp_CusId"));
+                customerConsultResponse.setCustomerId(rs.getString("customerId"));
+                customerConsultResponse.setName(rs.getString("name"));
+                customerConsultResponse.setPhoneNumber(rs.getString("phoneNumber"));
+                customerConsultResponse.setKindOfInsurance(KindOfInsurance.getKindOfInsuranceBy(rs.getInt("kindOfInsurance")));
+                customerConsultResponse.setKindOfJob(KindOfJob.getKindOfJobBy(rs.getString("kindOfJob")));
+                customerList.add(customerConsultResponse);
+            }
+
+                return customerList;
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
         return null;
+    }
+
+    public void consultExcute(Employee employee, CustomerConsultResponse customerConsultResponse) {
+        ResultSet rs = null;
+        try {
+            String sql = "Select Emp_Cus.emp_CusId, Customer.customerId, Customer.name, Customer.phoneNumber, Customer.kindOfInsurance, Customer.kindOfJob" +
+                    " from Emp_Cus, Customer " +
+                    "where Customer.customerId = Emp_Cus.customerId and Emp_Cus.satisfaction is null;";
+            PreparedStatement st = this.connection.prepareStatement(sql);
+
+            rs = st.executeQuery();
+
+            ArrayList<CustomerConsultResponse> customerList = new ArrayList<>();
+            while (rs.next()){
+                CustomerConsultResponse customerConsultResponse1 = new CustomerConsultResponse();
+                customerConsultResponse1.setEmpCusId(rs.getString("emp_CusId"));
+                customerConsultResponse1.setCustomerId(rs.getString("customerId"));
+                customerConsultResponse1.setName(rs.getString("name"));
+                customerConsultResponse1.setPhoneNumber(rs.getString("phoneNumber"));
+                customerConsultResponse1.setKindOfInsurance(KindOfInsurance.getKindOfInsuranceBy(rs.getInt("kindOfInsurance")));
+                customerConsultResponse1.setKindOfJob(KindOfJob.getKindOfJobBy(rs.getString("kindOfJob")));
+                customerList.add(customerConsultResponse1);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
