502bdccd761b8e78d01e74c8b8c9225cd5777b53
kingchan223 <kokiyo97@naver.com>
2022-06-05 14:44:49 +0900

    customercomment 상수 문자열 constants로 빼기

--- src/global/util/constants/CustomerConstants.java
+++ src/global/util/constants/CustomerConstants.java
@@ -1,4 +1,55 @@
 package global.util.constants;
 
-public class CustomerConstants {
+public class CustomerConstants extends CommonConstants {
+    public static final String GREET = "님 안녕하세요!";
+    public static final String THANKS_EVALUATION = "님 만족도 평가에 참여해주셔서 감사합니다";
+    public static final String COMP_CONSULT_REQ = "님 상담 요청이 완료되었습니다. 빠른 시일 내에 연락드리겠습니다.";
+    public static final String COMP_JOINING = "님 회원가입이 완료되었습니다.";
+    public static final String INITIAL_MSG = "고객님 안녕하세요. 회원가입을 하셨다면 로그인을, 아직 회원이 아니라면 회원가입을 진행해주세요.\n상담사 연결은 3번을 눌러주세요!";
+    public static final String INITIAL_MENU = "1.로그인\n2.회원가입\n3.상담사 연결\n4.종료";
+
+    public static final String INITIAL_MENU_1 = "1. 상담사 연결하기";
+    public static final String INITIAL_MENU_2 = "2. 상담사 평가하기";
+    public static final String INITIAL_MENU_3 = "3. 가입된 보험 조회하기";
+    public static final String INITIAL_MENU_4 = "4. 보험급 납부내역";
+    public static final String INITIAL_MENU_5 = "5. 사고 처리 접수";
+    public static final String INITIAL_MENU_6 = "6. 보험 가입하기";
+    public static final String INITIAL_MENU_7 = "7. 보험금 청구하기";
+    public static final String INITIAL_MENU_8 = "8. 로그아웃";
+
+    public static final String JOINED_INSURANCE_MENU_1 = "1. 보험 해지하기";
+    public static final String JOINED_INSURANCE_MENU_2 = "2. "+GO_BACK;
+
+
+    public static final String AFTER_LOGIN_INTEREST_MENU_1 = "1. 상담사 연결하기";
+    public static final String AFTER_LOGIN_INTEREST_MENU_2 = "2. 상담사 평가하기";
+    public static final String AFTER_LOGIN_INTEREST_MENU_3 = "3. 보험 가입하기";
+    public static final String AFTER_LOGIN_INTEREST_MENU_4 = "4. 로그아웃";
+
+    public static final String PAYMENT_HISTORY_START = "-------------------보험금 납부 내역-------------------";
+    public static final String END_LINE = "--------------------------------------------------";
+
+    public static final String INSURANCE_JOIN_HISTORY_START = "-------------------보험 가입 내역-------------------";
+    public static final String INSURANCE_INTEREST_HISTORY_START = "-------------------관심 보험 내역-------------------";
+    public static final String INSURANCE_NUMBER = " 보험 번호 : ";
+    public static final String INSURANCE_NAME = " 보험 이름 : ";
+    public static final String INSURANCE_CATEGORY = " 보험 종류 : ";
+    public static final String INSURANCE_REWARD = " 보험 종류 : ";
+    public static final String PAYMENT_FEE = "  | 납부 금액 : ";
+    public static final String PAYDAY = " | 납부 일 : ";
+
+    public static final String SELECT_NUMBER_TO_JOIN = "가입할"+INSURANCE + SELECT_NUM;
+    public static final String CHECK_PAYER = "보험금 수익자가 본인이면 0, 아니면 1을 입력해주세요.";
+    public static final String CHECK_BENEFICIARY = "보험금 납부자가 본인이면 0, 아니면 1을 입력해주세요.";
+    public static final String ASK_INSURANCE_CATEGORY = "생명보험에 관심이 있으시다면 1을, 손해보험에 관심이 있으시다면 2를 입력:";
+
+    public static final String SATISFACTION = "만족도(0~10):";
+
+    public static final String JOB_1 = "1. 직장인";
+    public static final String JOB_2 = "2. 학생";
+    public static final String JOB_3 = "3. 주부";
+    public static final String JOB_4 = "4. 위 중 해당사항 없음";
+
+
+
 }
