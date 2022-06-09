f174467e9ff8815b701d89be7a952178ab89a96d
kyubeom <be_man@naver.com>
2022-06-09 18:33:22 +0900

    발표이후

--- src/global/util/constants/DataBaseConstants.java
+++ src/global/util/constants/DataBaseConstants.java
@@ -1,8 +1,6 @@
 package global.util.constants;
 
 public class DataBaseConstants {
-    public static final String URL = "jdbc:mysql://umcdb.cuiea55gxaoy.ap-northeast-2.rds.amazonaws.com:3306/Insurance";
-    public static final String USER = "admin";
-    public static final String PW = "sojung210";
+
     public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 }
